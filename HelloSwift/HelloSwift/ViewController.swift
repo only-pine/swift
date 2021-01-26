9a6c3d20367d670c190b508c26e97233cbcdc356
only-pine <65760535+only-pine@users.noreply.github.com>
2021-01-26 23:30:25 +0900

    Do it Swift - 2장 예제
    Hello, Swift

---
+++ HelloSwift/HelloSwift/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by hansol on 2021/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+    
+}
+

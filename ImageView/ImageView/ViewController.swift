58588b39af4fb5f59a72c1253a22a12421c3dbb3
only-pine <65760535+only-pine@users.noreply.github.com>
2021-02-17 22:40:56 +0900

    Do it Swift - 3장 예제
    이미지 확대/축소, 켜짐/꺼짐 소스 추가 (완성)

--- ImageView/ImageView/ViewController.swift
+++ ImageView/ImageView/ViewController.swift
@@ -27,13 +27,35 @@ class ViewController: UIViewController {
         imgOff = UIImage(named: "lamp_off.png")
         
         //스토리보드에 추가한 이미지 뷰의 아웃렛 변수 imgView에 imgOn 이미지를 지정함
-        imgView.image = imgOn
+        imgView.image = imgOff
     }
 
+    //확대 버튼을 클릭했을 때, 동작하는 함수
     @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale : CGFloat = 2.0   //이미지를 확대할 배율값
+        var newWidth:CGFloat, newHeight:CGFloat //확대/축소했을때, 이미지의 가로/세로 길이를 저장하는 변수
+        if (isZoom) {   //확대되어 있는 경우
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+        }
+        else {          //축소되어 있는 경우
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+            
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
     }
     
+    //스위치 클릭했을 때, 동작하는 함수
     @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
     }
 }
 
